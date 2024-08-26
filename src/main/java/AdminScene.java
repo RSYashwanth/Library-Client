9a3899fdc1e27018d5e9ec5db8a7dc86be22ab60
RSYashwanth <yashwanthrs1104@gmail.com>
2024-08-26 14:46:05 -0500

    Modified AdminScene to add functionality

--- src/main/java/AdminScene.java
+++ src/main/java/AdminScene.java
@@ -7,7 +7,11 @@ import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.time.LocalDate;
 
@@ -21,16 +25,19 @@ public class AdminScene {
 
     private static Label numUsersLabel;
 
+    private static Scene scene;
+
     public static Scene getScene() {
         initializeLists();
         populateUI();
-        return new Scene(root, 600, 400);
+        scene = new Scene(root, 600, 400);
+        return scene;
     }
 
     private static void populateUI() {
-        Label label1 = new Label("Username: ");
-        numBooksLabel = new Label("Books in stock: ");
-        numUsersLabel = new Label("Number of users: ");
+        Label label1 = new Label("Username: " + DataManager.user);
+        numBooksLabel = new Label("Books in stock: " + DataManager.numBooks);
+        numUsersLabel = new Label("Number of users: " + DataManager.numUsers);
         VBox labelsBox = new VBox(label1, numBooksLabel, numUsersLabel);
 
         ListView<String> listView1 = new ListView<>(users);
@@ -44,18 +51,29 @@ public class AdminScene {
 
         Button button2 = new Button("Add Book");
         button2.setPrefSize(75, 50);
+        button2.setOnAction(e -> addBookPopup(scene.getWindow()));
 
         Button button3 = new Button("Make Admin");
         button3.setPrefSize(75, 50);
+        button3.setOnAction(e -> makeAdmin(listView1.getSelectionModel().getSelectedItem()));
 
         Button button4 = new Button("Refresh");
         button4.setPrefSize(75, 50);
+        button4.setOnAction(e -> {
+            refreshUserList();
+            refreshBookList();
+            refreshLabels();
+        });
 
         HBox buttonsBox = new HBox(button1, button2, button3, button4);
 
         ListView<Book> listView2 = new ListView<>(books);
         listView2.setCellFactory(AdminScene::generateCellFactory);
         listView2.setPrefSize(300, 350);
+
+        button1.setOnAction(e -> issueBook(
+                listView1.getSelectionModel().getSelectedItem(),
+                listView2.getSelectionModel().getSelectedItem().getId()));
 
         VBox rightVBox = new VBox(buttonsBox, listView2);
         rightVBox.setPrefWidth(300);
@@ -72,7 +90,7 @@ public class AdminScene {
                     setText(null);
                 }
                 else {
-                    setText(item.getName());
+                    setText(item.getName() + " ( id: " + item.getId() + " )");
                 }
             }
         };
@@ -86,34 +104,93 @@ public class AdminScene {
     }
 
     private static void refreshUserList() {
-        items.clear();
+        users.clear();
         try {
-            String books[] = HTTPUtil.write(
-                    DataManager.serverIp + "/books?username=" + DataManager.user,
+            String response[] = HTTPUtil.write(
+                    DataManager.serverIp + "/users",
                     "GET",
                     "").replace("\"", "").split("\\|\\|");
 
-            for (String book : books) {
+            for (String user : response) {
+                user = user.trim().substring(1, user.length()-1);
+                String[] parts = user.split(",");
+                String username = parts[1].split(":")[1].trim();
+                users.add(username);
+            }
+        }
+        catch (IOException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        DataManager.numUsers = users.size();
+    }
+
+    private static void refreshBookList() {
+        books.clear();
+        try {
+            String response[] = HTTPUtil.write(
+                    DataManager.serverIp + "/books/available",
+                    "GET",
+                    "").replace("\"", "").split("\\|\\|");
+
+            for (String book : response) {
+                if (book.length() == 0) {
+                    continue;
+                }
                 book = book.trim().substring(1, book.length()-1);
                 String[] parts = book.split(",");
-                int id = Integer.parseInt(parts[0].split(":")[1].trim());
+                int id = Integer.valueOf(parts[0].split(":")[1].trim());
                 String title = parts[1].split(":")[1].trim();
                 String date = parts[3].split(":")[1].trim();
                 if (!date.equals("null")) {
-                    System.out.println(date.split("T")[0]);
                     LocalDate issued = LocalDate.parse(date.split("T")[0]);
                     LocalDate due = issued.plusDays(15);
-                    items.add(new Book(id, title, issued, due));
+                    books.add(new Book(id, title, issued, due));
+                }
+                else {
+                    books.add(new Book(id, title, null, null));
                 }
             }
         }
         catch (IOException | IllegalArgumentException e) {
             e.printStackTrace();
         }
-        DataManager.numBooks = items.size();
+        DataManager.numBooks = books.size();
     }
 
-    private static void refreshBookList() {
+    private static void refreshLabels() {
+        numBooksLabel = new Label("Books in stock: " + DataManager.numBooks);
+        numUsersLabel = new Label("Number of users: " + DataManager.numUsers);
+    }
 
+    private static void makeAdmin(String username) {
+        try {
+            JSONObject data = new JSONObject();
+            data.put("username:" + username);
+            HTTPUtil.write(DataManager.serverIp + "/admin", "POST", data.toString());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void issueBook(String username, int id) {
+        try {
+            JSONObject data = new JSONObject();
+            data.put("username:" + username);
+            data.put("id:" + id);
+            HTTPUtil.write(DataManager.serverIp + "/issueBook", "POST", data.toString());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addBookPopup(Window owner) {
+        Stage popup = new Stage();
+        popup.initModality(Modality.APPLICATION_MODAL);
+        popup.initOwner(owner);
+
+        popup.setTitle("Add new book");
+        popup.showAndWait();
     }
 }
